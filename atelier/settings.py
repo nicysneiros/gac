75765efa62e27105b0ebd2525311cb492f69513c
nicysneiros <nicolle.cysneiros@gmail.com>
2013-07-13 17:50:10 -0300

    Finalizacao da parte html do formulario de adicao de pedido

--- atelier/settings.py
+++ atelier/settings.py
@@ -23,7 +23,7 @@ DATABASES = {
         'NAME': 'gac',               # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
-        'PASSWORD': 'sibemol',
+        'PASSWORD': '1234',
         'HOST': 'localhost',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
