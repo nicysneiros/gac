6f86661958b5761a831d842b1109d9f40f52fe82
eaa3 <eaa3@cin.ufpe.br>
2013-07-13 16:50:32 -0300

    added comments

--- povoamento.py
+++ povoamento.py
@@ -1,5 +1,6 @@
 from core.models import *
 
+#para rodar, abra python manage.py shell, e digite execfile('povoamento.py')
 
 
 for i in range(0,5):
@@ -7,7 +8,6 @@ for i in range(0,5):
 	c = Cliente(i, 'nome%d'%i, 'email@email.com%d'%i,e.id, False)
 	p = Produto(i,150,c.id,'mm','categoria','abc/')
 	d = Despesa(i,10,'abc','desc',p.id)
-
 	e.save()
 	c.save()
 	p.save()
