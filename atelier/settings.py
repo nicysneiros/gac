612c0eefb36c44534b059248eafd1afaa58d8ef7
eaa3 <eaa3@cin.ufpe.br>
2013-07-15 21:16:11 -0300

    postgree heroku configs

--- atelier/settings.py
+++ atelier/settings.py
@@ -20,12 +20,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'gac',               # Or path to database file if using sqlite3.
+        'NAME': 'dacloc22v40tiu',#'gac',               # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'rtjrwqemufjpwt'#'postgres',
+        'PASSWORD': 'pXGhYyQyblLfRA1bDYWyN3fPdt',#'1234',
+        'HOST': 'ec2-174-129-204-172.compute-1.amazonaws.com',#'localhost',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
