a5301dbce2a704d32b38b896a7bc1f0d0cc28c0c
robertacmota <rcm4@cin.ufpe.br>
2013-07-27 16:27:05 -0300

    Inicio do preenchimento da tabela em cliente.html
    O que foi feito: todos os campos estão sendo preenchidos, com exceção
    do campo de remoção.

--- cliente/views.py
+++ cliente/views.py
@@ -9,8 +9,21 @@ from django.template import RequestContext
 import datetime
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+from cliente.models import Cliente
+from pedido.models import Pedido
+from produto.models import Produto
+
 def cliente(request):
-    return render(request, 'cliente.html', {})
+    # Pega todos os pedidos que ainda estao em aberto
+    # pedidos_ab = Pedido.objects.all().filter(prazo__gte=datetime.now())
+
+    # Pega o numero de produtos comprados de cada cliente
+    clientes = Cliente.objects.all()
+    for cliente in clientes:
+        cliente.produtos = Produto.objects.filter(cliente=cliente.id)
+        cliente.pedidos = Pedido.objects.filter(cliente=cliente.id, prazo__gte=datetime.now()) 
+    return render(request, 'cliente.html', {'clientes': clientes})
 
 def detalhe_cliente (request):
     return render(request, 'detalhe_cliente.html', {})
