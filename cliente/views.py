523e5fa7583d4c9c13c7073084509034027d7b06
rlcocin <rlc@cin.ufpe.br>
2013-07-27 18:29:58 -0300

    Login e logout feitos faltando terminar de integrar com a parte de front end.

--- cliente/views.py
+++ cliente/views.py
@@ -8,9 +8,16 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 import datetime
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import *
+from django.shortcuts import redirect
 
+
+@login_required(redirect_field_name='redirect_to')
 def cliente(request):
-    return render(request, 'cliente.html', {})
+        nome_usuario = request.user.get_username()   
+        return render(request, 'cliente.html', {'nome_usuario':nome_usuario})
 
+
+@login_required(redirect_field_name='redirect_to')
 def detalhe_cliente (request):
     return render(request, 'detalhe_cliente.html', {})
--- login/views.py
+++ login/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import redirect
+from django.contrib.auth import views
+from django.contrib.auth import *
+from django.shortcuts import render
+
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def login_user(request):
+    logado = request.user.is_authenticated()
+    if not logado:
+        return views.login(request)
+    else:
+        nome_usuario = request.user.get_username()   
+        return render(request, 'registration/login.html', {'nome_usuario':nome_usuario, 'logado':logado})  
+
+
+
