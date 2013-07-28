d13cb0b1bb579310885b293e716b5810adb9477e
Mano <mano@ubuntu.(none)>
2013-07-28 00:47:41 -0300

    adicionando senha do botmail de verdade

--- atelier/settings.py
+++ atelier/settings.py
@@ -5,7 +5,7 @@ import os
 ROOT_PATH = os.path.dirname(__file__)
 MAIL_SERVER = "imap.gmail.com"
 BOTMAIL_USER = "botmail.sem.atrito@gmail.com"
-BOTMAIL_PASSWORD = "muqueals0"
+BOTMAIL_PASSWORD = "muequals0"
 
 
 DEBUG = True
