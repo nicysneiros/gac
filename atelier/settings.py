d0ba70dd0882a9a7b1ceebb748fceaf8872ef77c
rlcocin <rlc@cin.ufpe.br>
2013-08-25 20:51:38 -0300

    Relatorio Financeiro praticamente finalizado.

--- atelier/settings.py
+++ atelier/settings.py
@@ -148,6 +148,7 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'geraldo',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
