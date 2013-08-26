345d7717ad74a40111bb3798a75969ff505a63f9
rlcocin <rlc@cin.ufpe.br>
2013-08-25 22:50:39 -0300

    Ajeitando ReportLab

--- reportlab/graphics/__init__.py
+++ reportlab/graphics/__init__.py
@@ -1,1 +1,6 @@
-../../../../../share/pyshared/reportlab/graphics/__init__.py
+#Copyright ReportLab Europe Ltd. 2000-2012
+#see license.txt for license details
+#history http://www.reportlab.co.uk/cgi-bin/viewcvs.cgi/public/reportlab/trunk/reportlab/graphics/__init__.py
+__version__=''' $Id$ '''
+__doc__='''Framework for reusable object graphics, in PDF or bitmap form'''
+
